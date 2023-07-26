dbd42ae7847fba9b73ab188615c3c6bb646aa0d7
Aaditya Shukla <117642781+sam1276@users.noreply.github.com>
2023-07-26 21:06:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const profileIcon = document.querySelector('.profile-icon');
-    const dropdownContent = document.querySelector('.dropdown-content');
 
-    profileIcon.addEventListener('click', function() {
-        dropdownContent.classList.toggle('show');
-    });
 
-    document.addEventListener('click', function(event) {
-        const target = event.target;
-        if (!target.closest('.profile-icon')) {
-            dropdownContent.classList.remove('show');
-        }
-    });
-});
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const profileIcon = document.querySelector('.profile-icon');
+//     const dropdownContent = document.querySelector('.dropdown-content');
+
+//     profileIcon.addEventListener('click', function() {
+//         dropdownContent.classList.toggle('show');
+//     });
+
+//     document.addEventListener('click', function(event) {
+//         const target = event.target;
+//         if (!target.closest('.profile-icon')) {
+//             dropdownContent.classList.remove('show');
+//         }
+//     });
+// });
